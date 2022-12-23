a091975cbb1ec9155b425512288cca0145fec8dd
Büşra Yumuşakkaya <busrayumusakkaya@gmail.com>
2022-12-24 02:45:17 +0300

    Yeni Şifre düzenlendi

--- GlutensizYasam.UI/frmSifremiUnuttumEkrani.Designer.cs
+++ GlutensizYasam.UI/frmSifremiUnuttumEkrani.Designer.cs
@@ -35,74 +35,130 @@
             this.txtParolaYeniden = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // txtMailGiris
             // 
-            this.txtMailGiris.Location = new System.Drawing.Point(175, 34);
+            this.txtMailGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMailGiris.Location = new System.Drawing.Point(290, 86);
+            this.txtMailGiris.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMailGiris.Multiline = true;
             this.txtMailGiris.Name = "txtMailGiris";
-            this.txtMailGiris.Size = new System.Drawing.Size(237, 24);
+            this.txtMailGiris.Size = new System.Drawing.Size(315, 29);
             this.txtMailGiris.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(91, 37);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(32, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.Size = new System.Drawing.Size(119, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Email Giriniz";
             // 
             // btnParolaGuncelle
             // 
-            this.btnParolaGuncelle.Location = new System.Drawing.Point(175, 160);
+            this.btnParolaGuncelle.BackColor = System.Drawing.Color.Orange;
+            this.btnParolaGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnParolaGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnParolaGuncelle.ForeColor = System.Drawing.Color.White;
+            this.btnParolaGuncelle.Location = new System.Drawing.Point(290, 224);
+            this.btnParolaGuncelle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnParolaGuncelle.Name = "btnParolaGuncelle";
-            this.btnParolaGuncelle.Size = new System.Drawing.Size(237, 27);
+            this.btnParolaGuncelle.Size = new System.Drawing.Size(316, 42);
             this.btnParolaGuncelle.TabIndex = 2;
             this.btnParolaGuncelle.Text = "Parolayı Güncelle";
-            this.btnParolaGuncelle.UseVisualStyleBackColor = true;
+            this.btnParolaGuncelle.UseVisualStyleBackColor = false;
             this.btnParolaGuncelle.Click += new System.EventHandler(this.btnParolaGuncelle_Click);
             // 
             // txtParola
             // 
-            this.txtParola.Location = new System.Drawing.Point(175, 77);
+            this.txtParola.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtParola.Location = new System.Drawing.Point(290, 139);
+            this.txtParola.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtParola.Multiline = true;
             this.txtParola.Name = "txtParola";
-            this.txtParola.Size = new System.Drawing.Size(237, 24);
+            this.txtParola.Size = new System.Drawing.Size(315, 29);
             this.txtParola.TabIndex = 0;
             // 
             // txtParolaYeniden
             // 
-            this.txtParolaYeniden.Location = new System.Drawing.Point(175, 116);
+            this.txtParolaYeniden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtParolaYeniden.Location = new System.Drawing.Point(290, 187);
+            this.txtParolaYeniden.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtParolaYeniden.Multiline = true;
             this.txtParolaYeniden.Name = "txtParolaYeniden";
-            this.txtParolaYeniden.Size = new System.Drawing.Size(237, 24);
+            this.txtParolaYeniden.Size = new System.Drawing.Size(315, 29);
             this.txtParolaYeniden.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 80);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(32, 142);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.Size = new System.Drawing.Size(184, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Yeni Şifrenizi Giriniz";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 119);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(32, 190);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(132, 13);
+            this.label3.Size = new System.Drawing.Size(246, 25);
             this.label3.TabIndex = 1;
             this.label3.Text = "Yeni Şifrenizi Tekrar Giriniz";
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::GlutensizYasam.UI.Properties.Resources.wheat_allergy_amber_icon_15413;
+            this.pictureBox2.Location = new System.Drawing.Point(21, 11);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(84, 71);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 36;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("MV Boli", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.Orange;
+            this.label12.Location = new System.Drawing.Point(95, 28);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(188, 46);
+            this.label12.TabIndex = 35;
+            this.label12.Text = "Glutensiz";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("MV Boli", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label5.Location = new System.Drawing.Point(275, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 46);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "Yasam";
+            // 
             // frmSifremiUnuttumEkrani
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(460, 227);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(613, 279);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.btnParolaGuncelle);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -110,9 +166,12 @@
             this.Controls.Add(this.txtParolaYeniden);
             this.Controls.Add(this.txtParola);
             this.Controls.Add(this.txtMailGiris);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmSifremiUnuttumEkrani";
             this.Text = "frmSifremiUnuttumEkrani";
             this.Load += new System.EventHandler(this.frmSifremiUnuttumEkrani_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,5 +186,8 @@
         private System.Windows.Forms.TextBox txtParolaYeniden;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label5;
     }
 }
